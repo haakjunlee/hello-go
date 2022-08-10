c31aaa0f963dd06869f93bd0c903cc0177546d85
Hakjun Lee <haakjunlee@gmail.com>
2022-08-10 14:41:40 +0900

    chore: remove comments

--- hello/hello.go
+++ hello/hello.go
@@ -8,21 +8,13 @@ import (
 )
 
 func main() {
-	// Set properties of the predefined Logger, including
-	// the log entry prefix and a flag to disable printing
-	// the time, source file, and line number.
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	// Request a greeting message.
 	message, err := greetings.Hello("Hakjun")
-	// If an error was returned, print it to the console and
-	// exit the program.
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// If no error was returned, print the returned message
-	// to the console.
 	fmt.Println(message)
 }
